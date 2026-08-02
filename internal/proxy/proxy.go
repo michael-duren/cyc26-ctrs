3880295cac7e0ef7345ed0a584d41acb12477138
Michael Duren <michaeld@michaelduren.com>
2026-08-02 18:57:55 -0500

    feat: add proxy to network ns

---
+++ internal/proxy/proxy.go
@@ -0,0 +1,54 @@
+package proxy
+
+import (
+	"fmt"
+	"io"
+	"net"
+)
+
+func Proxy(addr1, addr2 string) error {
+	l, err := net.Listen("tcp", addr1)
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		logCloser("closing net.Listen", l.Close())
+	}()
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			return err
+		}
+
+		go func(c net.Conn) {
+			defer func() {
+				logCloser("closing host conn", c.Close())
+			}()
+			pconn, err := net.Dial("tcp", addr2)
+			if err != nil {
+				return
+			}
+			defer func() {
+				logCloser("closing addr2 conn", pconn.Close())
+			}()
+			go func() {
+				_, err := io.Copy(pconn, c)
+				if err != nil {
+					fmt.Println("error writing to ctr", err)
+				}
+			}()
+			_, err = io.Copy(c, pconn)
+			if err != nil {
+				fmt.Println("error writing back to host", err)
+			}
+		}(conn)
+	}
+}
+
+func logCloser(what string, err error) {
+	if err != nil {
+		fmt.Printf("%s error: %s\n", what, err)
+	}
+}
